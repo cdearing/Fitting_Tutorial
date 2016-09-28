596303c1bee69bf319dd6cfc127f240e58dba418
Corey Dearing <cdearing@live.unc.edu>
2016-09-28 17:34:02 -0400

    scatter update

--- scatter_estimators.py
+++ scatter_estimators.py
@@ -12,5 +12,9 @@ import numpy.random as npr
 
 xvals = np.linspace(1,10,100)
 yvals = np.linspace(20,40,100)
+errs = 1
+narr = 100
+
+yvals = yvals + npr.normal(0,errs,narr)
 
 plt.plot(xvals,yvals)
